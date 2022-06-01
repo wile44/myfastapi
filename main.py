779233dc745905a5d9bb31acb8803b2f708d0a9f
wile44 <goodluckwileonline@gmail.com>
2022-06-02 01:03:39 +0300

    CRUD routes

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-from typing import Union
-
 from fastapi import FastAPI
+import schemas
 
 app = FastAPI()
 
+fakeDatabase = {
+    1:{'task':'Clean car'},
+    2:{'task':'Write blog'},
+    3:{'task':'Start stream'},
+}
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def getItems():
+    return fakeDatabase
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/{id}}")
+def getItem(id: int):
+    return fakeDatabase[id]
+
+
+@app.post("/")
+def addItem(item: schemas.Item):
+    newItem = len(fakeDatabase.keys()) + 1
+    fakeDatabase[newItem] = {"task": item.task}
+    return fakeDatabase
+
+@app.put("/{id}")
+def updateItem(id: int, item: schemas.Item):
+    fakeDatabase[id] = {"task": item.task}
+    return fakeDatabase
+
+
+@app.delete("/{id}")
+def deleteItem(id: int):
+    del fakeDatabase[id] 
+    return fakeDatabase
