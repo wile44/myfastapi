56b96cf41e04bb03c65491f4590e93bc5d2f95b2
wile44 <goodluckwileonline@gmail.com>
2022-06-02 08:22:48 +0300

    add first item to the data base

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI, Depends, Body
+from fastapi import FastAPI, Body, Depends
 import schemas
 import models
 
 from database import Base, engine, SessionLocal
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session 
 
 Base.metadata.create_all(engine)
+
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
 
 app = FastAPI()
 
@@ -16,28 +23,52 @@ fakeDatabase = {
 }
 
 @app.get("/")
-def getItems():
-    return fakeDatabase
+def getItems(session: Session = Depends(get_session)):
+    items = session.query(models.Item).all()
+    return items
 
+@app.get("/{id}")
+def getItem(id:int, session: Session = Depends(get_session)):
+    item = session.query(models.Item).get(id)
+    return item
 
-@app.get("/{id}}")
-def getItem(id: int):
-    return fakeDatabase[id]
+#option #1
+# @app.post("/")
+# def addItem(task:str):
+#     newId = len(fakeDatabase.keys()) + 1
+#     fakeDatabase[newId] = {"task":task}
+#     return fakeDatabase
 
-
+#Option #2
 @app.post("/")
-def addItem(item: schemas.Item):
-    newItem = len(fakeDatabase.keys()) + 1
-    fakeDatabase[newItem] = {"task": item.task}
-    return fakeDatabase
+def addItem(item:schemas.Item, session: Session = Depends(get_session)):
+    item = models.Item(task = item.task)
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+
+    return item
+
+#Option #3
+# @app.post("/")
+# def addItem(body = Body()):
+#     newId = len(fakeDatabase.keys()) + 1
+#     fakeDatabase[newId] = {"task":body['task']}
+#     return fakeDatabase
+
 
 @app.put("/{id}")
-def updateItem(id: int, item: schemas.Item):
-    fakeDatabase[id] = {"task": item.task}
-    return fakeDatabase
+def updateItem(id:int, item:schemas.Item, session: Session = Depends(get_session)):
+    itemObject = session.query(models.Item).get(id)
+    itemObject.task = item.task
+    session.commit()
+    return itemObject
 
 
 @app.delete("/{id}")
-def deleteItem(id: int):
-    del fakeDatabase[id] 
-    return fakeDatabase
+def deleteItem(id:int, session: Session = Depends(get_session)):
+    itemObject = session.query(models.Item).get(id)
+    session.delete(itemObject)
+    session.commit()
+    session.close()
+    return 'Item was deleted...'
